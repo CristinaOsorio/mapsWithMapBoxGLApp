607b1bd4c75f1344f49db4a0ae3247c851dd6680
Cristina Osorio <48217224+CristinaOsorio@users.noreply.github.com>
2022-09-07 22:29:43 -0500

    Implementacion de un zoom personalizado en zoomRangeComponent

--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,25 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from "mapbox-gl";
 import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [
-  ]
+  styles: [`
+    .row {
+      background-color: white;
+      position: fixed;
+      border-radius: 5px;
+      left: 50px;
+      bottom: 50px;
+      padding: 10px;
+      z-index: 999;
+      width: 400px;
+    }
+  `]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
+  @ViewChild('map') divMap!: ElementRef;
+  map!: mapboxgl.Map;
+  zoomLevel = 10;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     (mapboxgl as any).accessToken = environment.mapboxToken;
-    const map = new mapboxgl.Map({
-      container: 'map',
+    this.map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-100.200588, 25.668650],
-      zoom: 17
+      zoom: this.zoomLevel
     });
+
+    this.map.on('zoom', (event) => {
+      this.zoomLevel = this.map.getZoom()
+    });
+
+    this.map.on('zoomend', (event) => {
+      if (this.map.getZoom() > 18) this.map.zoomTo(18);
+    });
+  }
+
+  zoomOut() {
+    this.map.zoomOut()
+  }
+
+  zoomIn() {
+    this.map.zoomIn()
+  }
+
+  zoomChange(value: string) {
+    this.map.zoomTo(Number(value));
   }
 
 }
