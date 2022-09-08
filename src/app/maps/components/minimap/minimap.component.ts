1127f1404dd00783a9fae076dd186fd23851d1c2
Cristina Osorio <48217224+CristinaOsorio@users.noreply.github.com>
2022-09-08 17:22:36 -0500

    Creacion de multiples mapas atraves de un componente reutilizable

--- src/app/maps/components/minimap/minimap.component.ts
+++ src/app/maps/components/minimap/minimap.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../../../../environments/environment';
+
 
 @Component({
   selector: 'app-minimap',
   templateUrl: './minimap.component.html',
-  styles: [
-  ]
+  styles: [` 
+    div {
+      width: 100%;
+      height: 150px;
+      margin: 0;
+    }
+  `]
 })
-export class MinimapComponent implements OnInit {
+export class MinimapComponent implements AfterViewInit {
+  @Input() lngLat!: [number, number];
+  @ViewChild('map') mapRef!: ElementRef;
+  map!: mapboxgl.Map;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+    this.map = new mapboxgl.Map({
+      container: this.mapRef.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+
+    const marker = new mapboxgl.Marker().setLngLat(this.lngLat).addTo(this.map)
   }
 
 }
